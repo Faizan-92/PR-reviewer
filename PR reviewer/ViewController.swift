a0c439e66253f7f6797976a7fe5a97991214ca0d
Faizan Memon <faizan@sharechat.co>
2022-10-05 09:54:06 +0530

    Initial Commit

---
+++ PR reviewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PR reviewer
+//
+//  Created by Faizan Memon on 05/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
